fc12a2d70816392f5cd5b414862d8f4d4bcd538d
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-06-20 14:25:09 +0600

    add here New question ad topic

--- Array/Array.js
+++ Array/Array.js
@@ -1,83 +1,33 @@
-// function minDays(bloomDay, m, k) {
-//   // Helper function to check if we can make m bouquets by day d
-//   function needFlowers(day) {
-//     let flowers = 0;
-//     let bouquets = 0;
-//     for (let i = 0; i < bloomDay.length; i++) {
-//       if (bloomDay[i] <= day) {
-//         flowers++;
-//         if (flowers === k) {
-//           bouquets++;
-//           flowers = 0;
-//         }
-//       } else {
-//         flowers = 0;
-//       }
-//       if (bouquets >= m) {
-//         return true;
+// const array = [2, 3, 4, 2, 5, 4];
+
+// function findDuplicate(data) {
+//   let duplicate = [];
+
+//   for (let i = 0; i < data.length; i++) {
+//     for (let j = i + 1; j < data.length; j++) {
+//       if (data[j] === data[i]) {
+//         duplicate.push(data[i]);
 //       }
 //     }
-//     return bouquets >= m;
 //   }
-
-//   if (bloomDay.length < m * k) {
-//     return -1; // Not enough flowers to make m bouquets
-//   }
-
-//   // Binary Search
-//   let low = Math.min(...bloomDay);
-//   let high = Math.max(...bloomDay);
-
-//   while (low < high) {
-//     const mid = Math.floor((low + high) / 2);
-//     if (needFlowers(mid)) {
-//       high = mid;
-//     } else {
-//       low = mid + 1;
-//     }
-//   }
-
-//   return low;
+//   return duplicate;
 // }
 
-// console.log(minDays([1, 10, 3, 9, 2, 4, 45, 56, 78, 10, 2], 3, 1));
+// console.log(findDuplicate(array));
+function findDuplicate(data) {
+  const seen = new Set(); // Use a Set for faster lookups
+  const duplicates = new Set();
 
-// console.log(`===============================`);
-// function minDays(bloomDay, m, k) {
-//   n = bloomDay.length;
+  for (const element of data) {
+    if (seen.has(element)) {
+      duplicates.add(element); // Found a duplicate
+    } else {
+      seen.add(element); // Add the element to the set
+    }
+  }
 
-//   if (n < m * k) return -1;
+  return Array.from(duplicates); // Convert Set to an array
+}
 
-//   low = Math.min(...bloomDay);
-//   high = Math.max(...bloomDay);
-//   bd = bloomDay;
-//   days = -1;
-
-//   while (low <= high) {
-//     mid = (low + high) >> 1;
-//     boqCnt = 0;
-
-//     adj = 0;
-//     for (let i = 0; i < bd.length; i++) {
-//       if (bd[i] <= mid) {
-//         adj++;
-//       } else {
-//         adj = 0;
-//       }
-
-//       if (adj == k) {
-//         boqCnt += 1;
-//         adj = 0;
-//       }
-//     }
-
-//     if (boqCnt >= m) {
-//       days = mid;
-//       high = mid - 1;
-//     } else {
-//       low = mid + 1;
-//     }
-//   }
-
-//   return days;
-// }
+const array = [2, 3, 4, 2, 5, 4];
+console.log(findDuplicate(array)); // Output: [2, 4]
