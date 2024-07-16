91e1887bc4578fa85443f67798041f247c640804
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-07-16 08:08:10 +0600

    solve 2 question

--- Array/Array.js
+++ Array/Array.js
@@ -1,33 +1,37 @@
-// const array = [2, 3, 4, 2, 5, 4];
-
-// function findDuplicate(data) {
-//   let duplicate = [];
-
-//   for (let i = 0; i < data.length; i++) {
-//     for (let j = i + 1; j < data.length; j++) {
-//       if (data[j] === data[i]) {
-//         duplicate.push(data[i]);
+// let array = [0, -1, 2, -3, 1];
+// let x = -2;
+// const pairOfSum = (arr, target) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === target) {
+//         console.log("Yes");
+//         return;
 //       }
 //     }
 //   }
-//   return duplicate;
-// }
+//   console.log("No");
+// };
 
-// console.log(findDuplicate(array));
-function findDuplicate(data) {
-  const seen = new Set(); // Use a Set for faster lookups
-  const duplicates = new Set();
+// // pairOfSum(array, x
 
-  for (const element of data) {
-    if (seen.has(element)) {
-      duplicates.add(element); // Found a duplicate
-    } else {
-      seen.add(element); // Add the element to the set
+// // Test cases
+// pairOfSum([0, -1, 2, -3, 1], -2); // Output: Yes
+// pairOfSum([1, -2, 1, 0, 5], 0); // Output: No
+
+// Two Sum using Naive Approach:
+
+const checkPair = (array, tar) => {
+  const arr = [...array];
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] + arr[j] === tar) {
+        console.log(`i found the target ${tar}`);
+        return;
+      }
     }
   }
+  console.log("no found this number ");
+};
 
-  return Array.from(duplicates); // Convert Set to an array
-}
-
-const array = [2, 3, 4, 2, 5, 4];
-console.log(findDuplicate(array)); // Output: [2, 4]
+console.log(checkPair([1, 4, 45, 6, 10, -8], 100));
---
+++ slove.js
@@ -0,0 +1,31 @@
+function findDuplicate(data) {
+  let duplicate = [];
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+      if (data[j] === data[i]) {
+        duplicate.push(data[i]);
+      }
+    }
+  }
+  return duplicate;
+}
+
+console.log(findDuplicate(array));
+function findDuplicate(data) {
+  const seen = new Set(); // Use a Set for faster lookups
+  const duplicates = new Set();
+
+  for (const element of data) {
+    if (seen.has(element)) {
+      duplicates.add(element); // Found a duplicate
+    } else {
+      seen.add(element); // Add the element to the set
+    }
+  }
+
+  return Array.from(duplicates); // Convert Set to an array
+}
+
+const array = [2, 3, 4, 2, 5, 4];
+console.log(findDuplicate(array)); // Output: [2, 4]
